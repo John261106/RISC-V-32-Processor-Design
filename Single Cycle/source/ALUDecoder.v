55eee66ccd1076aebd708c3415b684ac27ff1c5b
JANJANAM-KEDARANANDA <kedaranandajanjanam@gmail.com>
2025-09-20 20:38:16 +0530

    added ALUdecoder

--- Single Cycle/source/ALUDecoder.v
+++ Single Cycle/source/ALUDecoder.v
@@ -6,19 +6,27 @@ input wire [1:0] ALUOp,
 output reg [2:0] ALUControl
 );
 
+//supports lw,sw,beq,add,sub,slt,or,and
+//blindly followed DDCA Harris and Harris
+
 always @(*) begin
     case (ALUOp)
-        2'b00: ALUControl = 3'b010;
-        2'b01: ALUControl = 3'b110;
+        2'b00: ALUControl = 3'b000; //add
+        2'b01: ALUControl = 3'b001; //sub
         2'b10: begin
             case (funct3)
-                3'b000: ALUControl = (funct7b5 ? 3'b110 : 3'b010);
-                3'b111: ALUControl = 3'b000;
-                3'b110: ALUControl = 3'b001;
-                3'b100: ALUControl = 3'b011;
-                3'b010: ALUControl = 3'b111;
-                3'b001: ALUControl = 3'b100;
-                3'b101: ALUControl = (funct7b5 ? 3'b111 : 3'b101);
+                3'b000: begin
+                case ({op, funct7})
+                    2'b00 : ALUControl = 3'b000; //add
+                    2'b01 : ALUControl = 3'b000; //add
+                    2'b10 : ALUControl = 3'b000; //add
+                    2'b11 : ALUControl = 3'b001; //sub
+                endcase
+                end
+
+                3'b010: ALUControl = 3'b101; //set less than
+                3'b110: ALUControl = 3'b011; //or
+                3'b111: ALUControl = 3'b010; //and
                 default: ALUControl = 3'b000;
             endcase
         end
