29d9618cfb5848c9bfa5e700bec029e1be55a7c8
John Bobby <johnbobby2006@gmail.com>
2025-09-20 14:41:30 +0530

    Added ALU tb and modified ALU

--- Single Cycle/source/ALU.v
+++ Single Cycle/source/ALU.v
@@ -1,36 +1,25 @@
-//ALU
 module ALU(
-input wire [31:0] SrcA,
-input wire [31:0] SrcB,
-input wire [2:0] ALUControl,
-output reg [31:0] ALUResult,
-output reg Zero
+    input wire [31:0] SrcA,
+    input wire [31:0] SrcB,
+    input wire [2:0] ALUControl,
+    output reg [31:0] ALUResult,
+    output reg Zero
 );
 
-always@(*) begin
-//for ALU control unit
-if(ALUResult == 32'b0 ) begin
-Zero = 1;
+always @(*) begin
+    case(ALUControl)
+        3'b000 : ALUResult = SrcA + SrcB;
+        3'b001 : ALUResult = SrcA - SrcB;
+        3'b111 : ALUResult = SrcA & SrcB;
+        3'b011 : ALUResult = SrcA | SrcB;
+        3'b101 : ALUResult = (SrcA < SrcB) ? 1 : 0;
+        default : ALUResult = 32'b0;
+	// deafult case is zero
+    endcase
+
+    
+    Zero = (ALUResult == 32'b0) ? 1 : 0;
 end
-
-else begin
-Zero = 0;
-end
-
-case(ALUControl)
-    3'b000 : ALUResult = SrcA + SrcB;
-    3'b001 : ALUResult = SrcA - SrcB;
-    3'b111 : ALUResult = SrcA & SrcB;
-    3'b011 : ALUResult = SrcA | SrcB;
-    3'b101 : ALUResult = (SrcA < SrcB) ? 1 : 0
-    //credits to vighnesh for this mind blowing ass instruction
-    default : ALUResult = SrcA + SrcB;
-    //should we put default as zero ?
-endcase
-
-
-end
-
-
 
 endmodule
+
