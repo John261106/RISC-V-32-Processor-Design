0f55dd1954e07d0d149b505ed16626a8f4897f44
Rishi <rishi.iithyd@gmail.com>
2025-10-18 00:25:09 +0530

    Update InstructionMemory.v

--- Single Cycle/verification/InstructionMemory.v
+++ Single Cycle/verification/InstructionMemory.v
@@ -4,10 +4,8 @@ module InstructionMemory(
 );
 
 always @(*) begin
-    case (A[31:2])  // Use upper 30 bits (ignore lower 2 bits for word alignment)
-        30'h0: RD = 32'h00108133; // add x2, x1, x1
-        30'h1: RD = 32'h0020A023; // sw x2, 0(x1)
-        default: RD = 32'h00000013;
+    case (A[31:0]) 
+        32'h0: RD = 32'b11111111110001001010001100000011; // add x2, x1, x1
     endcase
 end
 
