3f4ac3ef5ba4799f34a4f55cd70a5180860addd7
Rishi <rishi.iithyd@gmail.com>
2025-10-18 00:26:41 +0530

    Update InstructionMemory.v

--- Single Cycle/verification/InstructionMemory.v
+++ Single Cycle/verification/InstructionMemory.v
@@ -5,7 +5,7 @@ module InstructionMemory(
 
 always @(*) begin
     case (A[31:0]) 
-        32'h0: RD = 32'b11111111110001001010001100000011; // add x2, x1, x1
+        32'h0: RD = 32'b11111111110001001010001100000011; // lw x6, -4(x9)
     endcase
 end
 
