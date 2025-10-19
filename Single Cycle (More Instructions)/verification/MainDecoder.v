8b5573c6379f32f1299b7ad768df490023f71eaf
John Bobby <johnbobby2006@gmail.com>
2025-10-19 18:16:51 +0530

    Chnanged the main decoder to accomodate ItypeALU instructions and jal, jalr left need

--- Single Cycle (More Instructions)/verification/MainDecoder.v
+++ Single Cycle (More Instructions)/verification/MainDecoder.v
@@ -1,6 +1,6 @@
 module MainDecoder(
 input wire [6:0] op,
-output reg ResultSrc,
+output reg[1:0] ResultSrc,//result src will become 2 bits to accomodate jal instruction
 output reg MemWrite,
 output reg [1:0] ALUOp,
 output reg ALUSrc,
@@ -11,7 +11,6 @@ output reg Branch
 );
 
 always@(*) begin
-//note : main decoder is designed to implemment lw,sw,beq
 //main deocer just  depends on op
 case (op)
         //lw
@@ -21,7 +20,7 @@ case (op)
             MemWrite = 1'b0;
             ALUOp = 2'b00;
             ALUSrc = 1'b1; //imm
-            ResultSrc =1'b1; //write back
+            ResultSrc =2'b01; //write back
             Branch = 1'b0;
             //KING LORD WORD USES ALL THE CONTROLS :) hehe
         end
@@ -34,7 +33,8 @@ case (op)
             ALUOp = 2'b00;
             ALUSrc = 1'b1; //imm
             Branch = 1'b0;
-            //ResultSrc = 1'bx are we supposed to do like this or just leave
+            ResultSrc=2'bxx;
+            //ResultSrc = 2'bxx are we supposed to do like this or just leave
         end
 
         // R-Type - ADD,OR,AND,SUB,LST
@@ -44,9 +44,35 @@ case (op)
             ALUOp = 2'b10;
             Branch = 1'b0;
             ALUSrc = 1'b0; //reg2
-            ResultSrc = 1'b0; //alu_result
+            ResultSrc = 2'b00; //alu_result
+            ImmSrc=2'bxx;
             //ImmSrc = 2'bxx are we supposed to do like this or just leave
         end
+
+        // I-Type ALU instructions
+        7'b0010011: begin 
+            RegWrite =1b'1;
+            MemWrite =1'b0;
+            ALUOp =2'b10; //doesnt matter becuase we modified the ALU decoder will see about this later
+            Branch =1'b0;
+            ALUSrc =1'b1;
+            ResultSrc=2'b00;
+            ImmSrc= 2'b00;
+        end
+
+
+        //jal instruction
+        7'b1101111: begin
+            RegWrite=1'b1;
+            MemWrite=1'b0;
+            ALUOp =2'b11 //doesnt matter because of the same reason
+            Branch = 1'b1;
+            ALUSrc= 1'b1;
+            ResultSrc =2'b10;
+        end
+
+        //need to add jalr instruction //hardware modification needed
+
 
         //beq
         7'b1100011 : begin
@@ -56,7 +82,8 @@ case (op)
             MemWrite = 1'b0;
             ALUOp = 2'b01;
             Branch = 1'b1;
-            //ResultSrc = 1'bx are we supposed to do like this or just leave
+            ResultSrc=2'bxx;
+            //ResultSrc = 2'bxx are we supposed to do like this or just leave
         end
 
     endcase
