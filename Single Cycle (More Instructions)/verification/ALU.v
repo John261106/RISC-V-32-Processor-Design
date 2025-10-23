cc0685ce334141472eec44741cd9ac1cb6fd1164
John Bobby <johnbobby2006@gmail.com>
2025-10-23 12:14:59 +0530

    fixed ALU decoder

--- Single Cycle (More Instructions)/verification/ALU.v
+++ Single Cycle (More Instructions)/verification/ALU.v
@@ -9,23 +9,19 @@ module ALU(
 
 always @(*) begin
     case(ALUControl)
-        3'b000 : ALUResult = SrcA + SrcB; //DONE
-        3'b001 : ALUResult = SrcA - SrcB; //DONE
-        3'b111 : ALUResult = SrcA & SrcB; //DONE
-        3'b011 : ALUResult = SrcA | SrcB; //DONE
-        3'b101 : ALUResult = (SrcA < SrcB) ? 1 : 0; //DONE
-        3'b100 : ALUResult = SrcA << SrcB; //DONE
-        3'b110 : ALUResult = SrcA >> SrcB; //DONE
-        3'b010 : ALUResult = SrcA ^ SrcB; //DONE
+        3'b000 : ALUResult = SrcA + SrcB;                              // ADD
+        3'b001 : ALUResult = SrcA - SrcB;                              // SUB
+        3'b111 : ALUResult = SrcA & SrcB;                              // AND
+        3'b011 : ALUResult = SrcA | SrcB;                              // OR
+        3'b101 : ALUResult = ($signed(SrcA) < $signed(SrcB)) ? 1 : 0;  // SLT (signed)
+        3'b100 : ALUResult = SrcA << SrcB[4:0];                        // SLL(only need to take 4:) since there are only 32 bits
+        3'b110 : ALUResult = SrcA >> SrcB[4:0];                        // SRL (logical)
+        3'b010 : ALUResult = SrcA ^ SrcB;                              // XOR
         default : ALUResult = 32'bx;
-	// deafult case is zero
     endcase
 
-    Negative = ~((SrcA - SrcB >= 0) ? 1 : 0);
-    Zero = (ALUResult == 32'b0) ? 1 : 0;
-    
+    Zero = (ALUResult == 32'b0);
+    Negative = ALUResult[31];
 end
 
 endmodule
-
-//checked
