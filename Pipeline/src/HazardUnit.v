df53f45c69ffc3426cf62206778f020be46aa4ae
RishikeshBavireddy <rishi.iithyd@gmail.com>
2025-11-10 18:53:02 +0530

    added inputs, outputs

--- Pipeline/src/HazardUnit.v
+++ Pipeline/src/HazardUnit.v
@@ -1,7 +1,16 @@
 module HazardUnit(
+input wire [31:0] Rs1EH,
+input wire [31:0] Rs2EH,
+input wire [31:0] RdMH,
+input wire [31:0] RdWH,
+input wire [31:0] RegWriteMH,
+input wire [31:0] RegWriteWH,
+
+output reg [1:0] ForwardAE,
+output reg [1:0] ForwardBE
+);
 
 
 
 
-
-)
+endmodule
